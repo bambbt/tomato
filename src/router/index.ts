eef3d5ac127c3b7d4d07abf93271bfbf3b2ec92d
bamb <bamb.bakang@gmail.com>
2019-10-16 21:04:31 -0400

    First commit. I restart the project using the vue cli. This time I will do some type script and some Vuex with the state management pattern.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,45 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import AddFoodVue from "../views/AddFood.vue";
+import AboutVue from "../views/About.vue";
+import SuggestionVue from "../views/Suggestion.vue";
+import KitchenVue from "../views/Kitchen.vue";
+import FavoritesVue from "../views/favorites.vue";
 
 Vue.use(VueRouter);
 
+
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  { path: "/suggestions", component: SuggestionVue },
+  { path: "/myKitchen", component: KitchenVue },
+  { path: "/scanFood", component: AddFoodVue },
+  { path: "/favoritesRecipes", component: FavoritesVue },
+  { path: "/about", component: AboutVue }
 ];
+
+
+// const routes = [
+//   {
+//     path: "/",
+//     name: "home",
+//     component: Home
+//   },
+//   {
+//     path: "/about",
+//     name: "about",
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/About.vue")
+//   }
+// ];
 
 const router = new VueRouter({
   mode: "history",
